91edd79792609225a8ba8fe4cf5c402d6bd90514
Dan Miller <tatebrasel@gmail.com>
2021-08-12 17:07:13 -0700

    update calcRocStats to work with generic data

--- R/calcRocStats.R
+++ R/calcRocStats.R
@@ -2,17 +2,16 @@
 #'
 #' @title Calculate ROC Statistics
 #'
-#' @description Calculates ROC statistics from a set of wetland-classified data
+#' @description Calculates ROC statistics for a set of classes and their
+#' probabilities data
 #' and a wetland probability raster. ROC statistics include: ROC, area under ROC
 #' curve (AUC), precision, accuracy.
 #'
-#' @param wetProbRaster A \code{SpatRaster} object where each cell value
-#' represents the probability of a wetland being there.
-#' @param classifiedPoints A \code{??} object of points which are classified as
-#' either "wetland" or "non-wetland" with the class names \code{wetlandClass}
-#' and \code{nonWetlandClass}.
-#' @param wetlandClass Name of the class indicating wetlands.
-#' @param nonWetlandClass Name of the class indicating non-wetlands.
+#' @param classes A character vector of classifications.
+#' @param probs A numeric vector of probability values corresponding to the
+#' elements of \code{classes}.
+#' @param posClass Name of the positive class.
+#' @param negClass Name of the negative class.
 #'
 #' @return A list of ROC statistic objects and values.
 #'
@@ -20,54 +19,29 @@
 #' \donttest{
 #' library(TerrainWorksUtils)
 #'
-#' wetProbRaster <- terra::rast("C:/Work/netmapdata/Puyallup/puy_prob.tif")
+#' # Generate test data with a bit of class probability overlap
+#' classes <- c(rep("WET", 40), rep("UPL", 60))
+#' probs <- c(runif(40, 0.4, 1.0), runif(60, 0.0, 0.6))
 #'
-#' allPoints <- terra::vect("C:/Work/netmapdata/Puyallup/wetlandPnts.shp")
-#' classifiedPoints <- allPoints[, "NEWCLASS"]
-#'
-#' rocStats <- calcRocStats(wetProbRaster, classifiedPoints, "WET", "UPL")
+#' rocStats <- calcRocStats(classes, probs, "WET", "UPL")
 #' }
 
 calcRocStats <- function(
-  wetProbRaster = NULL,
-  classifiedPoints = NULL,
-  wetlandClass = NULL,
-  nonWetlandClass = NULL
+  classes = NULL,
+  probs = NULL,
+  posClass = NULL,
+  negClass = NULL
 ) {
 
   # Validate parameters --------------------------------------------------------
-
-  # Get test point values ------------------------------------------------------
-
-  # Determine each point's wetland probability value
-  pointValues <- terra::extract(wetProbRaster, classifiedPoints, method = "simple")
-
-  # Include point classification values
-  pointValues["class"] <- terra::values(classifiedPoints)
-
-  # Remove point "ID" column
-  pointValues <- pointValues[,-1]
-
-  # Remove points with NA values
-  pointValues <- stats::na.omit(pointValues)
-
-  # Remove points that aren't classified as either "wetland" or "non-wetland"
-  wetlandPointIndices <- pointValues$class == wetlandClass
-  nonWetlandPointIndices <- pointValues$class == nonWetlandClass
-  pointValues <- pointValues[wetlandPointIndices | nonWetlandPointIndices,]
-
-  # Convert point class values (strings) to factors
-  pointValues$class <- factor(pointValues$class)
-
-  names(pointValues)[1] <- "wetProb"
 
   # Calculate ROC statistics ---------------------------------------------------
 
   # Consolidate points' wetland probabilities and classifications
   prediction <- ROCR::prediction(
-    predictions = pointValues$wetProb,
-    labels = pointValues$class,
-    label.ordering = c(wetlandClass, nonWetlandClass)
+    predictions = probs,
+    labels = classes,
+    label.ordering = c(negClass, posClass)
   )
 
   # Calculate the ROC curve
@@ -76,32 +50,30 @@ calcRocStats <- function(
   # Calculate the area under the ROC curve (AUC)
   auc <- ROCR::performance(prediction, measure = "auc")
 
-  # Calculate the precision
+  # Calculate the max precision and its cutoff point
   precision <- ROCR::performance(prediction, measure = "prec", x.measure = "rec")
+  maxPrecisionIndex <- which.max(methods::slot(precision, "y.values")[[1]])
+  prbe <- methods::slot(precision, "y.values")[[1]][maxPrecisionIndex]
+  maxPrecisionCutoff <- methods::slot(precision, "x.values")[[1]][maxPrecisionIndex]
 
-  # Calculate the accuracy
+  # Calculate the max accuracy and its cutoff point
   accuracy <- ROCR::performance(prediction, measure = "acc")
-
-  idx <- which.max(methods::slot(precision, "y.values")[[1]])
-  prbe <- methods::slot(precision, "y.values")[[1]][idx]
-  precisionCutoff <- methods::slot(precision, "x.values")[[1]][idx]
-
-  idx <- which.max(methods::slot(accuracy, "y.values")[[1]])
-  maxAccuracy <- methods::slot(accuracy, "y.values")[[1]][idx]
-  accuracyCutoff <- methods::slot(accuracy, "x.values")[[1]][idx]
+  maxAccuracyIndex <- which.max(methods::slot(accuracy, "y.values")[[1]])
+  maxAccuracy <- methods::slot(accuracy, "y.values")[[1]][maxAccuracyIndex]
+  maxAccuracyCutoff <- methods::slot(accuracy, "x.values")[[1]][maxAccuracyIndex]
 
   # Return ---------------------------------------------------------------------
 
   # Store ROC stats in a list
   rocStats <- list(
     roc = roc,
-    auc = auc,
     precision = precision,
     accuracy = accuracy,
     prbe = prbe,
-    precisionCutoff = precisionCutoff,
+    maxPrecisionCutoff = maxPrecisionCutoff,
     maxAccuracy = maxAccuracy,
-    accuracyCutoff = accuracyCutoff
+    maxAccuracyCutoff = maxAccuracyCutoff,
+    auc = auc
   )
 
   return(rocStats)
