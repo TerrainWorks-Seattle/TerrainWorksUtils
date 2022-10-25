8a8ae377fa5f3efa3f632fe2af7bebe275bc4fe2
Julia <julia.lober@outlook.com>
2022-10-25 15:16:22 -0600

    un-commented tests

--- tests/testthat/test-surface_metrics.R
+++ tests/testthat/test-surface_metrics.R
@@ -1,192 +1,192 @@
-# test_that("elev_deriv: build input file, all derivatives", {
-#   data_path <- system.file("examples", package = "TerrainWorksUtils")
-#   dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#
-#   rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"),
-#                      paste0("PLAN CURVATURE,", scratch_dir, "/plan"),
-#                      paste0("PROFILE CURVATURE,", scratch_dir, "/prof"),
-#                      paste0("NORMAL SLOPE CURVATURE,", scratch_dir, "/norm"),
-#                      paste0("TANGENTIAL CURVATURE,", scratch_dir, "/tan"))
-#
-#
-#   out_raster <- elev_deriv(rasters = rasters_input,
-#                            length_scale = 15,
-#                            dem = dem_path,
-#                            scratch_dir = scratch_dir)
-#
-#   expect_equal(nlyr(out_raster), 5)
-#   expect_equal(names(out_raster), c("GRADIENT",
-#                                     "PLAN CURVATURE",
-#                                     "PROFILE CURVATURE",
-#                                     "NORMAL SLOPE CURVATURE",
-#                                     "TANGENTIAL CURVATURE"))
-#   expect_true(file.exists(paste0(scratch_dir,"/grad.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/plan.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/prof.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/norm.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/tan.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/makegrids_input.txt")))
-#   unlink(paste0(scratch_dir,"\\*"))
-# })
-#
-# test_that("elev_deriv: build input file, 1 derivative", {
-#   data_path <- system.file("examples", package = "TerrainWorksUtils")
-#   dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#
-#
-#   rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"))
-#
-#
-#   out_raster <- elev_deriv(rasters = rasters_input,
-#                            length_scale = 15,
-#                            dem = dem_path,
-#                            scratch_dir = scratch_dir)
-#
-#   expect_equal(nlyr(out_raster), 1)
-#   expect_equal(names(out_raster), c("GRADIENT"))
-#   expect_true(file.exists(paste0(scratch_dir,"/grad.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/makegrids_input.txt")))
-#   unlink(paste0(scratch_dir,"\\*"))
-# })
-#
-# test_that("elev_deriv: length scale too small (<= 0)", {
-#   data_path <- system.file("examples", package = "TerrainWorksUtils")
-#   dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#
-#
-#   rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"))
-#
-#
-#   expect_error(elev_deriv(rasters = rasters_input,
-#                            length_scale = -1,
-#                            dem = dem_path,
-#                            scratch_dir = scratch_dir),
-#                "Length scale not specified or out-of-bounds")
-#   expect_error(elev_deriv(rasters = rasters_input,
-#                           length_scale = 0,
-#                           dem = dem_path,
-#                           scratch_dir = scratch_dir),
-#                "Length scale not specified or out-of-bounds")
-# })
-#
-# test_that("elev_deriv: existing input file, no other args", {
-#   data_path <- system.file("examples", package = "TerrainWorksUtils")
-#   input_file <- paste0(data_path, "\\makegrids_input_sample.txt")
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#   unlink(paste0(scratch_dir,"\\*"))
-#
-#   out_raster <- elev_deriv(input_file = input_file)
-#
-#   expect_equal(nlyr(out_raster), 5)
-#   expect_equal(names(out_raster), c("GRADIENT",
-#                                     "PLAN CURVATURE",
-#                                     "PROFILE CURVATURE",
-#                                     "NORMAL SLOPE CURVATURE",
-#                                     "TANGENTIAL CURVATURE"))
-#   expect_true(file.exists(paste0(scratch_dir,"/grad.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/plan.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/prof.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/norm.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/tan.flt")))
-#   unlink(paste0(scratch_dir,"\\*"))
-# })
-#
-# test_that("elev_deriv: existing input file, ignore other inputs", {
-#   data_path <- system.file("examples", package = "TerrainWorksUtils")
-#   input_file <- paste0(data_path, "\\makegrids_input_sample.txt")
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#   unlink(paste0(scratch_dir,"\\*"))
-#
-#   rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"),
-#                      paste0("PLAN CURVATURE,", scratch_dir, "/plan"),
-#                      paste0("PROFILE CURVATURE,", scratch_dir, "/prof"))
-#
-#   out_raster <- elev_deriv(input_file = input_file,
-#                            rasters = rasters_input)
-#
-#   expect_equal(nlyr(out_raster), 5)
-#   expect_equal(names(out_raster), c("GRADIENT",
-#                                     "PLAN CURVATURE",
-#                                     "PROFILE CURVATURE",
-#                                     "NORMAL SLOPE CURVATURE",
-#                                     "TANGENTIAL CURVATURE"))
-#   expect_true(file.exists(paste0(scratch_dir,"/grad.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/plan.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/prof.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/norm.flt")))
-#   expect_true(file.exists(paste0(scratch_dir,"/tan.flt")))
-#   unlink(paste0(scratch_dir,"\\*"))
-# })
-#
-# test_that("elev_deriv: non-existent, empty, or bad input file", {
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#   nonexist_file <- tempfile(tmpdir = scratch_dir)
-#
-#   expect_error(elev_deriv(input_file = nonexist_file), "Input file not found")
-#
-#   file.create(nonexist_file)
-#   expect_error(elev_deriv(input_file = nonexist_file), "Input file is empty")
-#
-#   writeLines("bad format", nonexist_file)
-#   expect_error(elev_deriv(input_file = nonexist_file), "Bad input file format")
-#
-#   unlink(paste0(scratch_dir,"\\*"))
-# })
-#
-# test_that("elev_deriv: non-existent scratch dir", {
-#   data_path <- system.file("examples", package = "TerrainWorksUtils")
-#   dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#
-#   rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"))
-#
-#   expect_error(elev_deriv(rasters = rasters_input,
-#                           length_scale = 1,
-#                           dem = dem_path,
-#                           scratch_dir = paste0(scratch_dir,"bad")),
-#                "Scratch directory does not exist")
-#
-# })
-#
-# test_that("elev_deriv: no derivatives given", {
-#   data_path <- system.file("examples", package = "TerrainWorksUtils")
-#   dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#
-#   rasters_input <- list()
-#
-#   expect_error(elev_deriv(rasters = rasters_input,
-#                           length_scale = 1,
-#                           dem = dem_path,
-#                           scratch_dir = scratch_dir),
-#                "Must provide at least one derivative to calculate")
-#
-# })
-#
-# test_that("elev_deriv: read existing rasters", {
-#   data_path <- system.file("examples", package = "TerrainWorksUtils")
-#   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
-#
-#   rasters_input <- c(paste0("GRADIENT,", data_path, "/elev_scottsburg.flt"))
-#
-#   expect_error(elev_deriv(rasters = list(),
-#                           length_scale = 1,
-#                           scratch_dir = scratch_dir),
-#                "Must provide a DEM or an existing raster file to read")
-#
-#   out_raster <- elev_deriv(rasters = rasters_input)
-#
-#   expect_equal(nlyr(out_raster), 1)
-#   expect_equal(names(out_raster), "GRADIENT")
-#
-#   expect_error(elev_deriv(rasters = paste0("GRADIENT,", data_path, "/bad.flt"),
-#                           length_scale = 100),
-#                "All given files must exist to run in read mode")
-#
-# })
+test_that("elev_deriv: build input file, all derivatives", {
+  data_path <- system.file("examples", package = "TerrainWorksUtils")
+  dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+
+  rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"),
+                     paste0("PLAN CURVATURE,", scratch_dir, "/plan"),
+                     paste0("PROFILE CURVATURE,", scratch_dir, "/prof"),
+                     paste0("NORMAL SLOPE CURVATURE,", scratch_dir, "/norm"),
+                     paste0("TANGENTIAL CURVATURE,", scratch_dir, "/tan"))
+
+
+  out_raster <- elev_deriv(rasters = rasters_input,
+                           length_scale = 15,
+                           dem = dem_path,
+                           scratch_dir = scratch_dir)
+
+  expect_equal(nlyr(out_raster), 5)
+  expect_equal(names(out_raster), c("GRADIENT",
+                                    "PLAN CURVATURE",
+                                    "PROFILE CURVATURE",
+                                    "NORMAL SLOPE CURVATURE",
+                                    "TANGENTIAL CURVATURE"))
+  expect_true(file.exists(paste0(scratch_dir,"/grad.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/plan.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/prof.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/norm.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/tan.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/makegrids_input.txt")))
+  unlink(paste0(scratch_dir,"\\*"))
+})
+
+test_that("elev_deriv: build input file, 1 derivative", {
+  data_path <- system.file("examples", package = "TerrainWorksUtils")
+  dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+
+
+  rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"))
+
+
+  out_raster <- elev_deriv(rasters = rasters_input,
+                           length_scale = 15,
+                           dem = dem_path,
+                           scratch_dir = scratch_dir)
+
+  expect_equal(nlyr(out_raster), 1)
+  expect_equal(names(out_raster), c("GRADIENT"))
+  expect_true(file.exists(paste0(scratch_dir,"/grad.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/makegrids_input.txt")))
+  unlink(paste0(scratch_dir,"\\*"))
+})
+
+test_that("elev_deriv: length scale too small (<= 0)", {
+  data_path <- system.file("examples", package = "TerrainWorksUtils")
+  dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+
+
+  rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"))
+
+
+  expect_error(elev_deriv(rasters = rasters_input,
+                           length_scale = -1,
+                           dem = dem_path,
+                           scratch_dir = scratch_dir),
+               "Length scale not specified or out-of-bounds")
+  expect_error(elev_deriv(rasters = rasters_input,
+                          length_scale = 0,
+                          dem = dem_path,
+                          scratch_dir = scratch_dir),
+               "Length scale not specified or out-of-bounds")
+})
+
+test_that("elev_deriv: existing input file, no other args", {
+  data_path <- system.file("examples", package = "TerrainWorksUtils")
+  input_file <- paste0(data_path, "\\makegrids_input_sample.txt")
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+  unlink(paste0(scratch_dir,"\\*"))
+
+  out_raster <- elev_deriv(input_file = input_file)
+
+  expect_equal(nlyr(out_raster), 5)
+  expect_equal(names(out_raster), c("GRADIENT",
+                                    "PLAN CURVATURE",
+                                    "PROFILE CURVATURE",
+                                    "NORMAL SLOPE CURVATURE",
+                                    "TANGENTIAL CURVATURE"))
+  expect_true(file.exists(paste0(scratch_dir,"/grad.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/plan.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/prof.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/norm.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/tan.flt")))
+  unlink(paste0(scratch_dir,"\\*"))
+})
+
+test_that("elev_deriv: existing input file, ignore other inputs", {
+  data_path <- system.file("examples", package = "TerrainWorksUtils")
+  input_file <- paste0(data_path, "\\makegrids_input_sample.txt")
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+  unlink(paste0(scratch_dir,"\\*"))
+
+  rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"),
+                     paste0("PLAN CURVATURE,", scratch_dir, "/plan"),
+                     paste0("PROFILE CURVATURE,", scratch_dir, "/prof"))
+
+  out_raster <- elev_deriv(input_file = input_file,
+                           rasters = rasters_input)
+
+  expect_equal(nlyr(out_raster), 5)
+  expect_equal(names(out_raster), c("GRADIENT",
+                                    "PLAN CURVATURE",
+                                    "PROFILE CURVATURE",
+                                    "NORMAL SLOPE CURVATURE",
+                                    "TANGENTIAL CURVATURE"))
+  expect_true(file.exists(paste0(scratch_dir,"/grad.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/plan.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/prof.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/norm.flt")))
+  expect_true(file.exists(paste0(scratch_dir,"/tan.flt")))
+  unlink(paste0(scratch_dir,"\\*"))
+})
+
+test_that("elev_deriv: non-existent, empty, or bad input file", {
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+  nonexist_file <- tempfile(tmpdir = scratch_dir)
+
+  expect_error(elev_deriv(input_file = nonexist_file), "Input file not found")
+
+  file.create(nonexist_file)
+  expect_error(elev_deriv(input_file = nonexist_file), "Input file is empty")
+
+  writeLines("bad format", nonexist_file)
+  expect_error(elev_deriv(input_file = nonexist_file), "Bad input file format")
+
+  unlink(paste0(scratch_dir,"\\*"))
+})
+
+test_that("elev_deriv: non-existent scratch dir", {
+  data_path <- system.file("examples", package = "TerrainWorksUtils")
+  dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+
+  rasters_input <- c(paste0("GRADIENT,", scratch_dir, "/grad"))
+
+  expect_error(elev_deriv(rasters = rasters_input,
+                          length_scale = 1,
+                          dem = dem_path,
+                          scratch_dir = paste0(scratch_dir,"bad")),
+               "Scratch directory does not exist")
+
+})
+
+test_that("elev_deriv: no derivatives given", {
+  data_path <- system.file("examples", package = "TerrainWorksUtils")
+  dem_path <- paste0(data_path, "\\elev_scottsburg.flt")
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+
+  rasters_input <- list()
+
+  expect_error(elev_deriv(rasters = rasters_input,
+                          length_scale = 1,
+                          dem = dem_path,
+                          scratch_dir = scratch_dir),
+               "Must provide at least one derivative to calculate")
+
+})
+
+test_that("elev_deriv: read existing rasters", {
+  data_path <- system.file("examples", package = "TerrainWorksUtils")
+  scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
+
+  rasters_input <- c(paste0("GRADIENT,", data_path, "/elev_scottsburg.flt"))
+
+  expect_error(elev_deriv(rasters = list(),
+                          length_scale = 1,
+                          scratch_dir = scratch_dir),
+               "Must provide a DEM or an existing raster file to read")
+
+  out_raster <- elev_deriv(rasters = rasters_input)
+
+  expect_equal(nlyr(out_raster), 1)
+  expect_equal(names(out_raster), "GRADIENT")
+
+  expect_error(elev_deriv(rasters = paste0("GRADIENT,", data_path, "/bad.flt"),
+                          length_scale = 100),
+               "All given files must exist to run in read mode")
+
+})
 
 test_that("contributing_area: build input file", {
   scratch_dir <- system.file("scratch", package = "TerrainWorksUtils")
