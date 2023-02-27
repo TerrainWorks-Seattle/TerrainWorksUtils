ea185b0ef77dc70f8cd20a21ebbecb9ab367f8a8
jlober01 <julia.lober@outlook.com>
2023-02-27 13:26:13 -0700

    added script for assembling model input dataframe

---
+++ R/create_model_dataframe.R
@@ -0,0 +1,34 @@
+# define where to find the data and load it.
+folder <- "/Users/julialober/Documents/terrainworks/code/sandbox/data/downloaded_2.27/out/"
+load(paste0(folder, "ls_1996.Rdata"))
+load(paste0(folder, "ls_2007.Rdata"))
+load(paste0(folder, "ls_2011.Rdata"))
+load(paste0(folder, "nonls_1996.Rdata"))
+load(paste0(folder, "nonls_2007.Rdata"))
+load(paste0(folder, "nonls_2011.Rdata"))
+
+# remove these lines when the bug is fixed. the $geo field in the 2011 and 2007
+# data frames is not stored as an integer value
+ls_2011$geo <- ls_2011$geo$GeoClass
+ls_2007$geo <- ls_2007$geo$GeoClass
+
+# add year labels to each selection of landslide points and non-ls points
+ls_1996$year <- rep(1996, length(ls_1996[, 1]))
+ls_2007$year <- rep(2007, length(ls_2007[, 1]))
+ls_2011$year <- rep(2011, length(ls_2011[, 1]))
+nonls_1996$year <- rep(1996, length(nonls_1996[, 1]))
+nonls_2007$year <- rep(2007, length(nonls_2007[, 1]))
+nonls_2011$year <- rep(2011, length(nonls_2011[, 1]))
+
+# combine into one data frame.
+subset96 <- 1:length(ls_1996[, 1])
+subset07 <- 1:length(ls_2007[, 1])
+subset11 <- 1:length(ls_2011[, 1])
+training_data <- rbind(ls_1996,
+                       ls_2007,
+                       ls_2011,
+                       nonls_1996[subset96, ],
+                       nonls_2007[subset07, ],
+                       nonls_2011[subset11, ])
+
+training_data$class <- as.factor(training_data$class)
