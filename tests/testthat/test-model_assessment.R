efbb87cce8ec2ec7dd62dff714fd5b22cbee72c4
jlober01 <julia.lober@outlook.com>
2023-06-05 12:50:33 -0600

    success_rate_curve testing setup

---
+++ tests/testthat/test-model_assessment.R
@@ -0,0 +1,11 @@
+test_that("success rate curve: bad inputs", {
+
+})
+
+test_that("success rate curve: returns a tibble", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("success rate curve: proportions range 0 to 1", {
+  expect_equal(2 * 2, 4)
+})
