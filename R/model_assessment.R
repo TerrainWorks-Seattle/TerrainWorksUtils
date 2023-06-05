9c5fac5b37c569444b850d2418bf698343577b47
jlober01 <julia.lober@outlook.com>
2023-06-05 12:49:38 -0600

    success_rate_curve function
    for generating a single success rate curve given some predictions

--- R/model_assessment.R
+++ R/model_assessment.R
@@ -12,37 +12,67 @@
 #' Success rate curves are generated using study areas.
 #' Prediction rate curves are generated on entire prediction areas - or new data.
 #'
-#' @param data
-#' @param bins
+#' @param data A data frame (ideally, a tibble) with probabilities, in this case, of landslide initiation.
+#' @param plot Change this value to FALSE if you do not want to produce the success rate curve plot.
+#' @param prob_col The name of the column which contains the probabilities.
+#' @param bins A vector with breaks for binning the cumulative probability sums. This is useful when combining multiple success rate curves, which might occur if a larger basin is divided into smaller sub-basin DEMs. If you do not want the curve to be binned, set this argument to NULL.
 #'
-#' @return
+#' @return A tibble with the success rate curve.
 #' @export
 #'
-#' @examples
 success_rate_curve <- function(data,
+                               plot = TRUE,
                                prob_col = "prob.pos",
-                               bins = NULL) {
+                               bins = seq(0, 1, length = 20)) {
 
-  if (is.null(data$prob_col)){
-    stop(" ")
+  # Check parameters ----------------------------------------------
+
+  if (is.null(data$prob_col)) {
+    stop("Check that prob_col exists in data.")
+  }
+  if (!is.bool(plot)) {
+    stop("Plot variable must be TRUE/FALSE.")
   }
 
+  # Calculate proportions -----------------------------------------
+
   # order the data by probability
-  proportions <- as.data.table(data)
-  setorder(proportions, cols = prob_col)
+  if (!is_tibble(data)) {
+    data <- as_tibble(data)
+  }
+  props <- data[c("row_ids", prob_col)]
+  names(props) <- c("row_ids", "prob")
+  props <- arrange(props, prob)
 
   # calculate the cumulative sums of area and probabilities
-  proportions$cumul_prob <- cumsum(proportions$prob.pos)
-  prob_sum <- max(proportions$prob.cumsum)
-  proportions$cumul_area <- seq(1, length(proportions$x))
-  area_sum <- max(proportions$area.cumsum)
+  props$prob_cumul <- cumsum(props$prob)
+  prob_sum <- max(props$prob_cumul)
+  props$area_cumul <- seq(1, length(props[[1]]))
+  area_sum <- max(props$area_cumul)
 
-  setattr(proportions, "sorted", "prob.pos")
+  props$prob_prop <- props$prob_cumul / prob_sum
+  props$area_prop <- props$area_cumul / area_sum
 
-  binned_prop <- proportions[J(bins$probability), roll = "nearest"]
-  cumul_prop$area <- cumul_prop$area + binned_prop$area.cumsum
-  cumul_prop$prob <- cumul_prop$prob + binned_prop$prob.cumsum
+  props_table <- as.data.table(props)
+  setattr(props_table, "sorted", "prob")
 
+  to_return <- props
 
-  return(data)
+  # Bin data ------------------------------------------------------
+
+  if (!is.null(bins) && is.vector(bins)) {
+    props_binned <- props_table[J(bins), roll = "nearest"]
+    to_return <- as_tibble(props_binned)
+  }
+
+  # Plot curve ----------------------------------------------------
+
+  ggplot(to_return, aes(x = area_prop, y = prob_prop)) +
+    geom_line() +
+    geom_point() +
+    labs(title = "Success rate curve",
+         x = "Proportion of area",
+         y = "Proportion of predicted landslides")
+
+  return(to_return)
 }
