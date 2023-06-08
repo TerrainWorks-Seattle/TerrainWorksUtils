75372537f2d20bde68429eed7b50bda598b4ad94
jlober01 <julia.lober@outlook.com>
2023-06-08 12:03:34 -0600

    add function for combining success rate curves

--- R/model_assessment.R
+++ R/model_assessment.R
@@ -12,6 +12,7 @@
 #' Success rate curves are generated using study areas.
 #' Prediction rate curves are generated on entire prediction areas - or new data.
 #'
+#'
 #' @param data A data frame (ideally, a tibble) with probabilities, in this case, of landslide initiation.
 #' @param plot Change this value to FALSE if you do not want to produce the success rate curve plot.
 #' @param prob_col The name of the column which contains the probabilities.
@@ -20,18 +21,18 @@
 #' @return A tibble with the success rate curve.
 #' @export
 #' @importFrom data.table data.table
-success_rate_curve <- function(data,
-                               plot = TRUE,
-                               prob_col = "prob.pos",
-                               bins = seq(0, 1, length = 20)) {
+srcurve <- function(data,
+                    plot = TRUE,
+                    prob_col = "prob.pos",
+                    bins = seq(0, 1, length = 20)) {
 
   # Check parameters ----------------------------------------------
 
   if (is.null(data)) {
     stop("No data supplied.")
   }
-  if (is.null(data[[prob_col]])) {
-    stop("prob_col must exist in data.")
+  if (!(prob_col %in% names(data))) {
+    stop("prob_col must exist in data or an alternate column name must be specified.")
   }
   if (!is.logical(plot)) {
     stop("Plot variable must be TRUE/FALSE.")
@@ -43,14 +44,15 @@ success_rate_curve <- function(data,
   if (!is_tibble(data)) {
     data <- as_tibble(data)
   }
-  props <- data[c("row_ids", prob_col)]
-  names(props) <- c("row_ids", "prob")
+  props <- data[c(prob_col)]
+  names(props) <- c("prob")
   props <- arrange(props, prob)
 
   # calculate the cumulative sums of area and probabilities
   props$prob_cumul <- cumsum(props$prob)
-  prob_sum <- max(props$prob_cumul)
   props$area_cumul <- seq(1, length(props[[1]]))
+
+  prob_sum <- max(props$prob_cumul)
   area_sum <- max(props$area_cumul)
 
   props$prob_prop <- props$prob_cumul / prob_sum
@@ -70,14 +72,81 @@ success_rate_curve <- function(data,
 
   # Plot curve ----------------------------------------------------
 
+  p <- ggplot(to_return, aes(x = area_prop, y = prob_prop)) +
+         geom_line() +
+         geom_point() +
+         labs(title = "Success rate curve",
+              x = "Proportion of area",
+              y = "Proportion of predicted landslides")
+
   if (plot) {
-    ggplot(to_return, aes(x = area_prop, y = prob_prop)) +
-      geom_line() +
-      geom_point() +
-      labs(title = "Success rate curve",
-           x = "Proportion of area",
-           y = "Proportion of predicted landslides")
+    print(p)
   }
 
   return(to_return)
+}
+
+#' Combine success rate curves
+#'
+#' This function combines success rate curves into one curve. This is a weighted average, since the curves could represent different amounts of data.
+#' The curves should be in the same format as produced by the srcurve() function provided in this package and all must be the same length.
+#'
+#' @param ... Any number of success rate curves to be combined.
+#' @param plot Change to FALSE if you do not want to produce a plot showing the curves.
+#'
+#' @return A tibble with the combined success rate curve.
+#' @export
+srcurve_combine <- function(...,
+                            plot = TRUE) {
+
+  # Check parameters ----------------------------------------------
+
+  curves <- list(...)
+  if (length(curves) == 0) {
+    stop("No data provided.")
+  }
+  first <- curves[[1]]
+  for (cur in curves) {
+    if (!all(c("prob_cumul", "area_cumul") %in% names(cur))) {
+      stop("All curves must have columns \"prob_cumul\" and \"area_cumul\".")
+    }
+    if (nrow(cur) != nrow(first)) {
+      stop("All curves must be the same length.")
+    }
+  }
+
+  # Combine curves ----------------------------------------------
+
+  sum_curve <- tibble(
+    prob_cumul = 0,
+    area_cumul = 0,
+    .rows = nrow(first)
+  )
+
+  for (cur in samp_list) {
+    sum_curve[["prob_cumul"]] <- sum_curve[["prob_cumul"]] + cur[["prob_cumul"]]
+    sum_curve[["area_cumul"]] <- sum_curve[["area_cumul"]] + cur[["area_cumul"]]
+  }
+
+  prob_sum <- max(sum_curve$prob_cumul)
+  area_sum <- max(sum_curve$area_cumul)
+
+  sum_curve$prob_prop <- sum_curve$prob_cumul / prob_sum
+  sum_curve$area_prop <- sum_curve$area_cumul / area_sum
+
+  print(sum_curve)
+
+  # Plot curve ----------------------------------------------------
+
+  p <- ggplot(sum_curve, aes(x = area_prop, y = prob_prop)) +
+        geom_line() +
+        geom_point() +
+        labs(title = "Success rate curve",
+             x = "Proportion of area",
+             y = "Proportion of predicted landslides")
+
+  if (plot) {
+    print(p)
+  }
+
 }
--- tests/testthat/test-model_assessment.R
+++ tests/testthat/test-model_assessment.R
@@ -1,14 +1,24 @@
 test_that("success rate curve: bad inputs", {
-  expect_error(success_rate_curve(NULL), "No data supplied")
-  expect_error(success_rate_curve(predictions, prob_col = "not_here"), "prob_col must exist in data.")
-  expect_error(success_rate_curve(predictions, plot = 0), "Plot variable must be TRUE/FALSE.")
+  expect_error(srcurve(NULL), "No data supplied")
+  expect_error(srcurve(predictions, prob_col = "not_here"), "prob_col must exist in data or an alternate column name must be specified.")
+  expect_error(srcurve(predictions, plot = -1), "Plot variable must be TRUE/FALSE.")
 })
 
-test_that("success rate curve: returns a tibble", {
-  res <- success_rate_curve(predictions)
-  # IN PROG --- currently, the predictions i am importing are generally very low,
-  # producing an almost right angle success rate curve. I may want to use a different model
-  # as an example.
+test_that("success rate curve: small tibble", {
+  input <- tibble(
+    row_ids = c(1, 2, 3, 4, 5),
+    prob.pos = c(1, 0, 0.5, .2, .2)
+  )
+  res <- srcurve(input, plot = FALSE)
+  expect_true(is_tibble(res))
+
+  # check that cumulative sums make sense
+  expect_equal(max(res[["prob_cumul"]]), sum(input[["prob.pos"]]))
+  expect_equal(max(res[["area_cumul"]]), max(input[["row_ids"]]))
+})
+
+test_that("success rate curve: test with sample dem", {
+  res <- srcurve(predictions, plot = FALSE)
   expect_true(is_tibble(res))
 
   # check that proportions range from 0 to 1
@@ -16,5 +26,13 @@ test_that("success rate curve: returns a tibble", {
   expect_true((1 - max(res[["prob_prop"]])) < 0.001)
   expect_true(min(res[["area_prop"]]) < 0.001)
   expect_true((1 - max(res[["area_prop"]])) < 0.001)
+})
+
+test_that("srcurve combine: bad inputs", {
+
+})
+
+test_that("srcurve combine: sum is right", {
+
 })
 
