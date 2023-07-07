5b7fdc6494dabefe727b02f4d47dcb293c059195
jlober01 <julia.lober@outlook.com>
2023-07-07 11:04:59 -0600

    finished predict_and_save

--- R/predict_dems.R
+++ R/predict_dems.R
@@ -17,7 +17,9 @@
 #     success rate curve function)
 
 predict_multiple_dems <- function(model,
+                                  pred_dir,
                                   basin_list,
+                                  out_dir = NULL,
                                   scale_vals = NULL,
                                   output = c("csv")) {
 
@@ -43,25 +45,49 @@ predict_multiple_dems <- function(model,
   # C/P from PFA_initiation_model.qmd
   # removed the success rate curve part, **started** to edit the code to adjust to parameters.
 
-  mapply()
+  if (is.null(out_dir)) {
+    out_dir <- paste0(dirname(pred_dir), "/predicting_output/")
+  }
 
-  predict_and_save(paste0(pred_dir, basin_list),
-                   file_out = paste0(pred_dir, basin_list, "predictions.csv"),
-                   model = ls_model,
-                   scale_vals = cs_vals,
-                   transform = make_quadratic_features)
+  mapply(predict_and_save,
+         paste0(pred_dir),
+         paste0(out_dir, basin_list, "/predictions.csv"),
+         MoreArgs = list(model = model,
+                         scale_vals = scale_vals,
+                         transform = make_quadratic_features))
+
+  # predict_and_save(paste0(pred_dir, basin_list),
+  #                  file_out = paste0(out_dir, basin_list, "/predictions.csv"),
+  #                  model = model,
+  #                  scale_vals = scale_vals,
+  #                  transform = make_quadratic_features)
 
 }
 
-# for use with lapply(basin_list, predict_and_save)
 
-# takes the dem location, the model,
+#' Predict and save
+#'
+#' This function takes a folder with elevation derivative files for a basin and a model and saves the predictions to a .csv file.
+#' The csv has columns "x" and "y" with coordinates, "prob.pos" with the model output, and all features used in the calculation of the model.
+#'
+#' TODO: add compression option
+#'
+#' @param dir_in A directory which contains the necessary elevation derivative files for prediction.
+#' @param file_out The name of the csv file to write results.
+#' @param model A trained model. Must have function $predict_newdata and $state$train_task$feature_names. These are both provided by most mlr3 learners.
+#' @param scale_vals The centering and scaling values used in training the model. The same values should be used when predicting data. The function expects a tibble with one column per feature and two values per feature. For example, \code{tibble(gradient = c(center_value, scale_value), mean_curv = c(center_value, scale_value))}.
+#' @param transform A function that calculates any transformed columns needed for the model. For example, this package includes a function called "make_quadratic_features" which calculateds squared and interaction terms for all numeric features.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 predict_and_save <- function(dir_in,
                              file_out,
                              model,
                              scale_vals = NULL,
                              transform = function(x) x) {
-
+  tic()
   # read files into a raster
   topo_files <- c(paste0("GRADIENT,", dir_in, "/gradient.flt"),
                   paste0("MEAN CURVATURE,", dir_in, "/mean_curv.flt"))
@@ -78,54 +104,42 @@ predict_and_save <- function(dir_in,
 
 
   input_data$pca <- log(input_data$pca)
-  input_data <- rename(input_data, ln_pca_k1_48 = pca)
+  input_data <- rename(input_data, ln_pca_k1_48 = pca) # comment this out when i change the names used for training the model.
 
   input_data <- transform(input_data)
-
-  # print(input_data)
-
-  # # check that transform() added all necessary features
-  # if (!all(names(input_data) %in% model$task$feature_names)) {
-  #   message <- paste0("Some features are missing: ", {{names(input_data)}})
-  #   stop("Some input features are missing: ")
-  # }
-
-  # print(scale_vals)
-  # print(names(input_data))
 
   # center and scale the data
   if (!is.null(scale_vals)) {
     for (col in names(scale_vals)) {
       if (col %in% names(input_data)) {
-        # print(col)
-        # print(scale_vals[1, col])
-        # print(scale_vals[2, col])
         input_data[[col]] <- ((input_data[[col]] - scale_vals[[col]][1]) / scale_vals[[col]][2])
-        # input_data[col] <- (input_data[col] - scale_vals[1, col]) / scale_vals[2, col]
       }
     }
   }
 
-  # print(input_data)
-
   # predict the data
   input_data$row_ids <- seq(1, length(input_data[[1]]))
-
-  # print(input_data$gradient)f
-
   predictions <- as.data.table(model$predict_newdata(input_data))
   predictions <- full_join(as_tibble(predictions), input_data,
                            by = "row_ids",
                            multiple = "error")
 
   # write to a file
-  to_select <- c("row_ids", "x", "y", "prob.pos", ls_model$state$train_task$feature_names)
+  to_select <- c("row_ids", "x", "y", "prob.pos", model$state$train_task$feature_names)
+  dir.create(dirname(dirname(file_out)), showWarnings = FALSE)
   dir.create(dirname(file_out), showWarnings = FALSE)
+
+  if (!str_ends(file_out, fixed(".csv"))) {
+    file_out <- ".csv"
+  }
+  # print(file_out)
   write_csv(select(predictions, to_select),
             file_out)
+  print(paste0("Wrote file ", file_out))
 
+  toc(quiet = FALSE)
   # print(predictions)
-  return(predictions)
+  return(TRUE)
 
 }
 
@@ -134,6 +148,7 @@ predict_and_save <- function(dir_in,
 #' This helper function adds quadratic features to add for training the model. TerrainWorks found that adding these features could improve model performance.
 #' Squared terms and interaction terms are added to the original data frame using using standardized column names.
 #'
+#' TODO: add handling for non-numeric features.
 #'
 #' @param data A data frame with feature columnns.
 #' @param ignore A list of column names to ignore. Default is ("x", "y", and "row_ids").
